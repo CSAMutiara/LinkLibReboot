a02a24e13c4df36cf55885ccaacb16d73dd6f0c8
Ayesha Mutiara <ayesha.mutiara@outlook.com>
2018-02-28 17:12:22 +0000

    remove favicon again

--- config/express.js
+++ config/express.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 module.exports = function() {
     let app = express();
-    // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
     // change of dir!
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'ejs');
